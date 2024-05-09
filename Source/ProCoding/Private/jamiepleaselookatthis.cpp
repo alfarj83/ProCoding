f4004bce63b4ce5dbf3bd06ab6732675e0e5e43c
laZy1806 <kylegosine@gmail.com>
2024-05-09 16:00:07 -0400

    jamie can you see this stuff

---
+++ Source/ProCoding/Private/jamiepleaselookatthis.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "jamiepleaselookatthis.h"
+
+jamiepleaselookatthis::jamiepleaselookatthis()
+{
+}
+
+jamiepleaselookatthis::~jamiepleaselookatthis()
+{
+}
