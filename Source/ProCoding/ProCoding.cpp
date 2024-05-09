ee7aa0af85cc6d4394ac353b0bd82830cc5cc295
alfarj83 <jemiapalo83@gmail.com>
2024-05-09 14:53:27 -0400

    Initial commit

---
+++ Source/ProCoding/ProCoding.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProCoding.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProCoding, "ProCoding" );
